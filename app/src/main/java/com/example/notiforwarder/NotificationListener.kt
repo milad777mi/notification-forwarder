30cbbb08ca3c4ab0decd929ce3b8d79ffecdbac4
mj9246N <mjfull603@gmail.com>
2026-07-25 22:01:41 +0330

    Create NotificationListener.kt

---
+++ app/src/main/java/com/example/notiforwarder/NotificationListener.kt
@@ -0,0 +1,45 @@
+package com.example.notiforwarder
+
+import android.app.Notification
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import kotlinx.coroutines.*
+
+class NotificationListener : NotificationListenerService() {
+
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.IO + job)
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+        sbn ?: return
+
+        val packageName = sbn.packageName
+        val extras = sbn.notification.extras
+        val title = extras.getString(Notification.EXTRA_TITLE) ?: ""
+        val text = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString() ?: ""
+        val time = sbn.postTime
+
+        val appName = getAppName(packageName)
+
+        scope.launch {
+            Sender.send(appName, packageName, title, text, time)
+        }
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification?) {}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
+    private fun getAppName(packageName: String): String {
+        return try {
+            val appInfo = packageManager.getApplicationInfo(packageName, 0)
+            packageManager.getApplicationLabel(appInfo).toString()
+        } catch (e: Exception) {
+            packageName
+        }
+    }
+}
