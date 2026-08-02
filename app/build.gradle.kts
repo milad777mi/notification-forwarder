3f9a77bbc8c37cfe1d3c787f1f5fb4068039be37
mj9246N <mjfull603@gmail.com>
2026-08-02 17:11:25 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.notiforwarder"
-        minSdk = 19
+        minSdk = 18
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
