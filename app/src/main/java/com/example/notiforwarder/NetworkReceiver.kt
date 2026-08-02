daa3d986ff0da588b1c875ef0fc90f7a2c3b67eb
mj9246N <mjfull603@gmail.com>
2026-08-02 16:44:05 +0330

    Update NetworkReceiver.kt

--- app/src/main/java/com/example/notiforwarder/NetworkReceiver.kt
+++ app/src/main/java/com/example/notiforwarder/NetworkReceiver.kt
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
+import android.net.NetworkInfo
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -19,8 +19,7 @@ class NetworkReceiver : BroadcastReceiver() {
 
     private fun isNetworkAvailable(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = cm.activeNetwork ?: return false
-        val caps = cm.getNetworkCapabilities(network) ?: return false
-        return caps.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
     }
 }
